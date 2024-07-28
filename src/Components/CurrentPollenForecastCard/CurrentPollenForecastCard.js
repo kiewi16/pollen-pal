88a5ef4076c693a3654a94413a997eb6c26a85a1
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-27 18:11:09 -0600

    adds props to component

--- src/Components/CurrentPollenForecastCard/CurrentPollenForecastCard.js
+++ src/Components/CurrentPollenForecastCard/CurrentPollenForecastCard.js
@@ -1,5 +1,10 @@
-function CurrentPollenForecastCard() {
-
+function CurrentPollenForecastCard({ currentPollenForecastData }) {
+    // const { name, value, category } = currentPollenForecastData
+    console.log("currentPollenForecastData in Card", currentPollenForecastData)
+    // console.log("name:", name)
+    // console.log("value:", value)
+    // console.log("category:", category)
+   
 }
 
 export default CurrentPollenForecastCard
