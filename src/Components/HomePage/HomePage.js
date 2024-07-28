65b96cc3b893b77cd6cf5b0ad6854fbb05e89af3
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-27 21:10:20 -0600

    adds styling to HomePage and CurrentPollentForecast

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,10 +1,13 @@
+import '../HomePage/HomePage.css'
 import { Link } from 'react-router-dom'
 
 function HomePage() {
     return (
-        <main>
-            <Link to ="/CurrentPollenForecast" className="current-pollen-forecast-link">Today's Pollen Forecast</Link>
-            <Link to ="/FiveDayPollenForecast" className="five-day-pollen-forecast-link">5-Day Pollen Forecast</Link>
+        <main className="homepage-wrapper">
+            <nav class='links-container'>
+                <Link to="/CurrentPollenForecast" className="current-pollen-forecast-link">Today's Pollen Forecast</Link>
+                <Link to="/FiveDayPollenForecast" className="five-day-pollen-forecast-link">5-Day Pollen Forecast</Link>
+            </nav>
             <p>This application is dedicated to my husband, an allergy sufferer, as a thank you for his unwavering support on my journey to becoming a software engineer!</p>
         </main>
     )
