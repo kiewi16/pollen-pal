0d89abedf6d722621b07c5908564488b0f53ef11
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-27 18:12:23 -0600

    adds getCurrentPollenForecast fetch function and useEffect that runs when the component is mounted. Passes props to CurrentPolenForecastCard in return.

--- src/Components/CurrentPollenForecast/CurrentPollenForecast.js
+++ src/Components/CurrentPollenForecast/CurrentPollenForecast.js
@@ -1,5 +1,38 @@
-function CurrentPollenForecast() {
+import { useState, useEffect } from 'react'
+import CurrentPollenForecastCard from '../CurrentPollenForecastCard/CurrentPollenForecastCard'
 
+function CurrentPollenForecast() {
+    const [currentPollenForecastData, setCurrentPollenForecast] = useState([])
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    function getCurrentPollenForecasts() {
+        // fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/337466?apikey=RlGJ3tQAAtATkTkWTQvIt9Mhy7FG2RS1&details=true')
+        // fetch('http://localhost:3001/api/v1/ideas')
+            .then(response => response.json())
+            // .then(data => console.log(data.DailyForecasts[0].AirAndPollen))
+            .then(data => setCurrentPollenForecast(data.DailyForecasts[0].AirAndPollen))
+            .catch(error => setErrorMessage(error.message))
+    }
+
+    useEffect(() => {
+        getCurrentPollenForecasts()
+    }, [])
+
+    // const currentPollenForecastCards = currentPollenForecastData.map(currentPollenForecast => {
+    //     return (
+    //         <CurrentPollenForecastCard
+    //             currentPollenForecast={currentPollenForecast}
+    //         />
+    //     )
+    // })
+
+    return (
+        <div className="current-pollen-forecast">
+            <h2>Today's Pollen Foreast for Highlands Ranch, Colorado</h2>
+            {errorMessage && <p> {errorMessage} </p>}
+            <CurrentPollenForecastCard currentPollenForecastData={currentPollenForecastData}/>
+        </div>
+    )
 }
 
 export default CurrentPollenForecast
