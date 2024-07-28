2e07995aa70153d6fd77649ce7aa99c471b2df8d
Kim Ewing <pekar.kimberly@gmail.com>
2024-07-27 19:37:47 -0600

    adds styling to the CurrentPollenForecast and CurrentPollenForecastCard

--- src/Components/CurrentPollenForecast/CurrentPollenForecast.js
+++ src/Components/CurrentPollenForecast/CurrentPollenForecast.js
@@ -1,3 +1,4 @@
+import '../CurrentPollenForecast/CurrentPollenForecast.css'
 import { useState, useEffect } from 'react'
 import CurrentPollenForecastCard from '../CurrentPollenForecastCard/CurrentPollenForecastCard'
 
@@ -6,10 +7,9 @@ function CurrentPollenForecast() {
     const [errorMessage, setErrorMessage] = useState(null)
 
     function getCurrentPollenForecasts() {
-        // fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/337466?apikey=RlGJ3tQAAtATkTkWTQvIt9Mhy7FG2RS1&details=true')
-        // fetch('http://localhost:3001/api/v1/ideas')
+        fetch('http://dataservice.accuweather.com/forecasts/v1/daily/1day/337466?apikey=RlGJ3tQAAtATkTkWTQvIt9Mhy7FG2RS1&details=true')
+            // fetch('http://localhost:3001/api/v1/ideas')
             .then(response => response.json())
-            // .then(data => console.log(data.DailyForecasts[0].AirAndPollen))
             .then(data => setCurrentPollenForecast(data.DailyForecasts[0].AirAndPollen))
             .catch(error => setErrorMessage(error.message))
     }
@@ -28,9 +28,11 @@ function CurrentPollenForecast() {
 
     return (
         <div className="current-pollen-forecast">
-            <h2>Today's Pollen Foreast for Highlands Ranch, Colorado</h2>
+            <h2>Today's Pollen Forecast for Highlands Ranch, Colorado</h2>
             {errorMessage && <p> {errorMessage} </p>}
-            {currentPollenForecastCards}
+            <div className="current-pollen-forecast-cards-wrapper">
+                {currentPollenForecastCards}
+            </div>
         </div>
     )
 }
--- src/Components/CurrentPollenForecastCard/CurrentPollenForecastCard.js
+++ src/Components/CurrentPollenForecastCard/CurrentPollenForecastCard.js
@@ -1,8 +1,21 @@
+import '../CurrentPollenForecastCard/CurrentPollenForecastCard.css'
+
 function CurrentPollenForecastCard({ currentPollenForecast }) {
     const { Name, Value, Category } = currentPollenForecast
 
+    const gradientStyles = {
+        Low: 'linear-gradient(to right, white, rgb(144, 238, 144)',
+        Moderate: 'linear-gradient(to right, #FFFFE0, #FFD700',
+        High: 'linear-gradient(to right, #FFD1DC, #FF4500)',
+        VeryHigh: 'linear-gradient(to right, #FFD1DC, #FF6347, #FF4500, #9b2226)',
+        Extreme: 'linear-gradient(to right, #f44336, #9b2226)'
+    }
+
+    const gradientStyle = gradientStyles[Category]
+    const cardStyle = gradientStyle ? { backgroundImage: gradientStyle } : {}
+
     return (
-        <div className="current-pollen-forecast-card">
+        <div className="current-pollen-forecast-card" style={cardStyle}>
             <h3>{Name}</h3>
             <p>{Value}</p>
             <p>{Category}</p>
